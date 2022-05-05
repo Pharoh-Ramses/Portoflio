3a595b5a4da8dcc9e5fcbbcec8b620e1c3293458
Ramses Angles <ramses@covidclinic.org>
2022-05-04 19:26:16 -0500

    Finished Sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,36 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom';
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboardCheck, faEnvelope, faHome, faRunning, faUser } from '@fortawesome/free-solid-svg-icons';
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo" />
+            <img className="sub-logo"src={LogoSubtitle} alt="slobodan" />
+        </Link>
+        <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="routines-link" to="/routines">
+            <FontAwesomeIcon icon={faClipboardCheck} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="activities-link" to="/activities">
+            <FontAwesomeIcon icon={faRunning} color="#4d4d4e" />
+        </NavLink>
+        </nav>
+        <ul>
+        <li>
+          <NavLink
+            exact="true" activeclassname="active" className='login-link' to="/login">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+)
+
+export default Sidebar;
